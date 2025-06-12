8be2de670a20741d39d98a6b27785c18a680aab4
ashwinramesh972 <ashwinramesh972@gmail.com>
2025-06-12 15:55:07 +0530

    Added Loginapp

--- backend/Lunariyo/Lunariyo/settings.py
+++ backend/Lunariyo/Lunariyo/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'APP_REGISTRATION',  
+    'APP_REGISTRATION',
+    'APP_LOGIN' 
 ]
 
 MIDDLEWARE = [
